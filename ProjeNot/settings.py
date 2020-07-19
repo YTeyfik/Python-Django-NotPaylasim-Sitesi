9b8c1a083a27142459edbf691c64ea701a78fb22
YTeyfik <teyo_2086@hotmail.com>
2020-07-19 19:04:06 +0300

    Note App Oluşturma ve Category Tablosu

--- ProjeNot/settings.py
+++ ProjeNot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Note.apps.NoteConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
