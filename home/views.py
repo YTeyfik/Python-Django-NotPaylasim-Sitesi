9ca83e9e6c002618640ec4fe3f6a2445395ac76c
YTeyfik <teyo_2086@hotmail.com>
2020-07-19 16:27:23 +0300

    Home App Oluşturma ve sonrası

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    text="Merhaba Django"
+    text="Django Kurulumu: python -m pipi install -e django <br> Proje oluşturma: django-admin startproject mysite <br> App Ekleme: python manage.py startapp polls"
     context={'text':text}
     return render(request,'index.html' ,context)
