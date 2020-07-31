ac8002fb38d7d6ed20f275901ce518d66ada90c1
YTeyfik <teyo_2086@hotmail.com>
2020-07-31 16:32:57 +0300

    Admin resim galerisi ve resimlerin gösterilmesi

--- Note/admin.py
+++ Note/admin.py
@@ -1,18 +1,31 @@
+
 from django.contrib import admin
 
 # Register your models here.
-from Note.models import Category, Note
+from Note.models import Category, Note, Images
 
+class NoteImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status','image_tag']
     list_filter = ['status']
+    readonly_fields = ('image_tag',)
 
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ['title','category','status']
+    list_display = ['title','category','status','image_tag']
     list_filter = ['status','category']
+    inlines = [NoteImageInline]
+    readonly_fields = ('image_tag',)
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display =['title','note','image_tag']
+    readonly_fields = ('image_tag',)
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Note,NoteAdmin)
+admin.site.register(Images,ImagesAdmin)
--- Note/models.py
+++ Note/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS=(
         ('True','Evet'),
@@ -19,19 +22,43 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def image_tag(self):
+        if self.image:
+            return mark_safe(f'<img src="{self.image.url}" height="50"/>')
+        else:
+            return ""
 class Note(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
     category=models.ForeignKey(Category,on_delete=models.CASCADE)#category tablosu ile ilişki
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True, upload_to='images/')
+    image = models.ImageField(blank=True,upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     detail=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe(f'<img src="{self.image.url}" height="50"/>')
+        else:
+            return ""
+
+class Images(models.Model):
+    note=models.ForeignKey(Note,on_delete=models.CASCADE) #cascade alt elemanları dahil
+    title = models.CharField(max_length=50,blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe(f'<img src="{self.image.url}" height="50"/>')
+        else:
+            return ""
