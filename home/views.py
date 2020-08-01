c3bb3ac7578107548a8b2f1423b50a38ad1921b2
YTeyfik <teyo_2086@hotmail.com>
2020-08-01 19:31:21 +0300

    Contact Formu Yapıldı

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
+
+
 # Create your views here.
 def index(request):
     setting= Setting.objects.get(pk=1)
@@ -17,6 +20,20 @@ def reference(request):
     context={'setting':setting}
     return render(request, 'reference.html', context)
 def contact(request):
+    if request.method=='POST':
+        form=ContactFormu(request.POST)
+        if form.is_valid():
+            data=ContactFormMessage()
+            data.name=form.cleaned_data['name']
+            data.email=form.cleaned_data['email']
+            data.subject=form.cleaned_data['subject']
+            data.message=form.cleaned_data['message']
+            data.ip=request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız başarı ile gönderilmiştir.Teşekkür Ederiz")
+            return  HttpResponseRedirect('/contact')
+
     setting= Setting.objects.get(pk=1)
-    context={'setting':setting}
+    form=ContactFormu()
+    context={'setting':setting,'form':form}
     return render(request, 'contact.html', context)
