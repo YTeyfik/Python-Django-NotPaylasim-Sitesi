b5247a4a7c2db7413f9dea894e5415a1b9443c0b
YTeyfik <teyo_2086@hotmail.com>
2020-08-07 20:03:06 +0300

    Kayıt Formu User Sign Up

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from Note.models import Note, Category, Images, Comment
-from home.forms import SearchForm
+from home.forms import SearchForm, SignUpForm
 from home.models import Setting, ContactFormu, ContactFormMessage
 
 
@@ -140,3 +140,23 @@ def login_view(request):
     }
 
     return render(request,'login.html',context)
+
+def signup_view(request):
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data.get('username')
+            password=form.cleaned_data.get('password1')
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return HttpResponseRedirect('/')
+
+    form=SignUpForm()
+    category = Category.objects.all()
+    context = {
+        'category': category,
+        'form':form,
+    }
+
+    return render(request,'signup.html',context)
