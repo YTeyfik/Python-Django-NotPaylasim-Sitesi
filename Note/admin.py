1815516ca993b0cb5869825b493bde13cd807e15
YTeyfik <teyo_2086@hotmail.com>
2020-08-07 13:23:26 +0300

    Kategori ve Not için otomatik slug oluşturma

--- Note/admin.py
+++ Note/admin.py
@@ -23,6 +23,7 @@ class NoteAdmin(admin.ModelAdmin):
     list_filter = ['status','category']
     inlines = [NoteImageInline]
     readonly_fields = ('image_tag',)
+    prepopulated_fields = {'slug': ('title',)}
 
 
 class ImagesAdmin(admin.ModelAdmin):
@@ -34,6 +35,7 @@ class CategoryAdmin2(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title',
                     'related_notes_count', 'related_notes_cumulative_count')
     list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug': ('title',)}
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
